eb7341d7f3c60b51ca58a69e9ea1c07333822996
kokomade98 <c0305sh@naver.com>
2020-08-02 14:20:30 +0900

    React-NodeBird Redux 구조 만들기

---
+++ React-NodeBird-FullStack/front/recuders/post.js
@@ -0,0 +1,37 @@
+export const initialState = {
+    mainPosts: [],
+};
+
+const ADD_POST = 'ADD_POST';
+const ADD_DUMMY = 'ADD_DUMMY';
+
+export const addPost = {
+    type: ADD_POST,
+};
+
+export const addDummy = {
+    type: ADD_DUMMY,
+    payload: {
+        content: 'Hello',
+        UserId: 1,
+        User: {
+            nickname: '코코하세요',
+        }
+    }
+};
+
+const reducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+        case ADD_POST: {
+            return {
+                ...state,
+                mainPosts: [...payload, ...state.mainPosts]
+            }
+        }
+        case ADD_DUMMY: {
+            return { ...state, }
+        }
+    }
+};
+
+export default reducer;
---
+++ React-NodeBird-FullStack/front/recuders/user.js
@@ -0,0 +1,62 @@
+/*  
+    Store
+    유저 정보 관리
+    intialState 에 넣은 값들은 초기값으로 설정된다.
+*/
+export const initialState = {
+    isLoggedIn: false,
+    user: {},
+};
+
+/*
+    Action
+    액션들의 이름
+*/
+const LOG_IN = 'LOG_IN';
+const LOG_OUT = 'LOG_OUT';
+
+/*
+    Action
+    실제 액션 ( 액션의 이름 + 넣어줄 데이터 )
+*/
+export const loginAction = {
+    type: LOG_IN,
+    payload: {
+        nickname: '코코하세요',
+    }
+};
+
+export const logoutAction = {
+    type: LOG_OUT,
+};
+
+/*
+    Reducer
+    parms -> state, action 받음.
+
+    주로 switch 구문을 사용하여 action.type로 구분하여 로직 생성.
+
+    state의 상태 관리를 위해 구조분해 문법을 사용하여 기존 데이터는 살리고
+    새로운 데이터를 생성해서 return 해 준다. ( 불변성을 위해서!! )
+*/
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOG_IN: {
+            return {
+                ...state,
+                isLoggedIn: true,
+                user: action.payload
+            }
+        }
+        case LOG_OUT: {
+            return {
+                ...state,
+                isLoggedIn: false,
+                user: null,
+            }
+        }
+        default :
+    }
+};
+
+export default reducer;
