20130a15ca61fe9978bde7cfe4d0f78b9852f16b
SeungHyun Cho <c0305sh@naver.com>
2020-09-08 00:54:26 +0900

    Redux state + action 구조 잡기

--- React-NodeBird-FullStack/front/recuders/user.js
+++ React-NodeBird-FullStack/front/recuders/user.js
@@ -12,8 +12,17 @@ const dummyUser = {
     intialState 에 넣은 값들은 초기값으로 설정된다.
 */
 export const initialState = {
-  isLoggedIn: false,
-  user: null,
+  isLoggedIn: false, // 로그인 여부
+  isLoggingOut: false, // 로그아웃 시도중
+  isLoggingIn: false, // 로그인 시도중
+  logInErrorReason: '', // 로그인 실패 사유
+  signedUp: false, // 회원가입 성공
+  isSigningUp: false, // 회원가입 시도중
+  signUpErrorReason: '', // 회원가입 실패 사유
+  me: null, // 내 정보
+  followingList: [], // 팔로잉 리스트
+  followerList: [], // 팔로워 리스트
+  userInfo: null, // 남의 정보
 };
 
 /*
@@ -28,9 +37,32 @@ export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
 export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
 export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
 
+export const LOAD_USER_REQUEST = 'LOAD_USER_REQUEST';
+export const LOAD_USER_SUCCESS = 'LOAD_USER_SUCCESS';
+export const LOAD_USER_FAILURE = 'LOAD_USER_FAILURE';
+
 export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
 export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
 export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
+
+// 팔로워 목록 불러오기
+export const LOAD_FOLLOW_REQUEST = 'LOAD_FOLLOW_REQUEST';
+export const LOAD_FOLLOW_SUCCESS = 'LOAD_FOLLOW_SUCCESS';
+export const LOAD_FOLLOW_FAILURE = 'LOAD_FOLLOW_FAILURE';
+
+export const FOLLOW_USER_REQUEST = 'FOLLOW_USER_REQUEST';
+export const FOLLOW_USER_SUCCESS = 'FOLLOW_USER_SUCCESS';
+export const FOLLOW_USER_FAILURE = 'FOLLOW_USER_FAILURE';
+
+export const UNFOLLOW_USER_REQUEST = 'UNFOLLOW_USER_REQUEST';
+export const UNFOLLOW_USER_SUCCESS = 'UNFOLLOW_USER_SUCCESS';
+export const UNFOLLOW_USER_FAILURE = 'UNFOLLOW_USER_FAILURE';
+
+export const REMOVE_FOLLOWER_REQUEST = 'REMOVE_FOLLOWER_REQUEST';
+export const REMOVE_FOLLOWER_SUCCESS = 'REMOVE_FOLLOWER_SUCCESS';
+export const REMOVE_FOLLOWER_FAILURE = 'REMOVE_FOLLOWER_FAILURE';
+
+export const ADD_POST_TO_ME = 'ADD_POST_TO_ME';
 
 // export const INCREMENT_NUMBER = "INCREMENT_NUMBER"; // 동기 요청
 
@@ -77,16 +109,23 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         isLoggedIn: true,
-        user: dummyUser,
+        me: dummyUser,
         loginData: action.data,
         isLoading: false,
+      };
+    }
+    case LOG_IN_FAILURE: {
+      return {
+        ...state,
+        isLoggedIn: false,
+        me: null,
       };
     }
     case LOG_OUT_REQUEST: {
       return {
         ...state,
         isLoggedIn: false,
-        user: null,
+        me: null,
       };
     }
     case SIGN_UP_REQUEST: {
--- React-NodeBird-FullStack/front/sagas/user.js
+++ React-NodeBird-FullStack/front/sagas/user.js
@@ -1,12 +1,49 @@
 import {
   all, call, fork, put, takeEvery, takeLatest,
 } from 'redux-saga/effects';
-import { LOG_IN_SUCCESS, LOG_IN_FAILURE, LOG_IN_REQUEST } from '../recuders/user';
+import axios from 'axios';
+import {
+  LOG_IN_SUCCESS, LOG_IN_FAILURE, LOG_IN_REQUEST, SIGN_UP_REQUEST, SIGN_UP_SUCCESS, SIGN_UP_FAILURE,
+} from '../recuders/user';
 
-const HELLO_SAGA = 'HELLO_SAGA';
+function signUpAPI() {
+  // 서버에 요청을 보내는 부분
+  return axios.post('/signup');
+}
+
+function* signUp() {
+  try {
+    yield call(signUpAPI);
+    yield put({ /* put 은 dispatch 와 동일하다. */
+      type: SIGN_UP_SUCCESS,
+    });
+    // loginAPI 실패
+  } catch (e) {
+    console.log(e);
+    yield put({
+      type: SIGN_UP_FAILURE,
+    });
+  }
+}
+
+function* watchSignUp() {
+  yield takeEvery(SIGN_UP_REQUEST, signUp);
+}
+
+/**
+ * takeLatest 의 역할
+ * 1. takeLatest 가 LOG_IN 액션이 dispatch 되길 기다린다.ff
+ * 2. dispatch 될 때 login 제너레이터를 호출한다.
+ * +) 여러번 같은 dispatch가 되어도 결과는 하나. ( EX 중복 로그인 요청 방지 )
+ *
+ * take 의 역할
+ * 해당 액션이 dispatch 되면 제너레이터를 next 하는 이펙트.
+ *
+ * while (true)를 걷어내고 takeEvery OR takeLatest 를 사용하여 적용. ( 기능은 같다 )
+ */
 
 function loginAPI() {
-  // 서버에 요청을 보내는 부분
+  return axios.post('/login');
 }
 
 function* login() {
@@ -24,42 +61,14 @@ function* login() {
   }
 }
 
-/**
- * takeLatest 의 역할
- * 1. takeLatest 가 LOG_IN 액션이 dispatch 되길 기다린다.ff
- * 2. dispatch 될 때 login 제너레이터를 호출한다.
- * +) 여러번 같은 dispatch가 되어도 결과는 하나. ( EX 중복 로그인 요청 방지 )
- *
- * take 의 역할
- * 해당 액션이 dispatch 되면 제너레이터를 next 하는 이펙트.
- */
 function* watchLogin() {
   yield takeEvery(LOG_IN_REQUEST, login);
 }
-
-// while (true)를 걷어내고 takeEvery OR takeLatest 를 사용하여 적용. ( 기능은 같다 )
-function* watchHello() {
-  yield takeLatest(HELLO_SAGA, function* () {
-    yield put({
-      type: 'BYE_SAGA',
-    });
-  });
-}
-
-// function* watchHello() {
-//     while (true) {
-//         yield take(HELLO_SAGA);
-//         console.log(1);
-//         console.log(2);
-//         console.log(3);
-//         console.log(4);
-//     }
-// }
 
 /* call : 동기, fork : 비동기 */
 export default function* userSaga() {
   yield all([
     fork(watchLogin),
-    fork(watchHello),
+    fork(watchSignUp),
   ]);
 }
